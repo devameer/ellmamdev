f9cfaf8e448883ba5434732184d4ce6566075b94
devameer <ameerafch@gmail.com>
2022-09-08 09:12:54 +0300

    fix bugs

--- invoice.php
+++ invoice.php
@@ -2,7 +2,7 @@
 if (isset($_GET['invoice_id']) && !empty($_GET['invoice_id'])) {
     include 'includes/functions.php';
     $invoice = getInvoiceById((int)$_GET['invoice_id']);
-    if($invoice == null){
+    if ($invoice == null) {
         include('404.php');
         exit;
     }
@@ -11,161 +11,77 @@ if (isset($_GET['invoice_id']) && !empty($_GET['invoice_id'])) {
 include 'includes/header.php';
 ?>
 
-<div class="single-page-head">
+<style>
+.navbar-offcanvas .logo img {
+    filter: brightness(20.5);
+}
+</style>
+<div class="template-page-head">
     <div class="container">
-        <div class="row" data-aos="fade-up" data-aos-delay="300">
-            <div class="col-lg-6">
+        <div class="row align-items-center" data-aos="fade-up" data-aos-delay="300">
+            <div class="col-6">
                 <div class="text">
-                    <h1>إستشارة تجريبية</h1>
-                    <div class="meta">
-                        <span>تكلفة الإستشارة للساعة <strong>50$ </strong></span>
-                        <!-- <span>بتاريخ <strong>8 يناير 2020</strong></span> -->
-                    </div>
+                    <h1>الدفع </h1>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="/ ">الرئيسية</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">
+                                الدفع
+                            </li>
+                        </ol>
+                    </nav>
                 </div>
             </div>
-            <div class="col-lg-6">
+            <div class="col-6">
                 <div class="img">
-                    <img src="/assets/images/logo-white.svg" alt=""/>
+                    <img src="/assets/images/payment.svg" alt="" />
                 </div>
             </div>
         </div>
     </div>
 </div>
-<div class="single-page-body">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12 col-lg-11 col-xl-10 col-xxl-9">
-                <div class="single-page-body-inner">
-                    <div class="single-page-preview" data-aos="fade-up" data-aos-delay="900">
-                        <div class="preview">
-                            <div class="image">
-                                <img src="https://hbrarabic.com/wp-content/uploads/2019/11/%D9%87%D8%A7%D8%B1%D9%81%D8%A7%D8%B1%D8%AF-%D8%A8%D8%B2%D9%86%D8%B3-%D8%B1%D9%8A%D9%81%D9%8A%D9%88-%D8%A7%D9%84%D8%AA%D9%86%D9%88%D8%B9-%D9%81%D9%8A-%D9%85%D9%83%D8%A7%D9%86-%D8%A7%D9%84%D8%B9%D9%85%D9%84.jpg"
-                                     alt=""/>
-                            </div>
-                        </div>
-                        <!-- <div class="social_share">
-                  <ul>
-                    <li>
-                      <a href="" class="facebook">
-                        <svg>
-                          <use
-                            xlink:href="/assets/images/icons.svg#facebook"
-                          ></use></svg
-                      ></a>
-                    </li>
-                    <li>
-                      <a href="" class="twitter">
-                        <svg>
-                          <use xlink:href="/assets/images/icons.svg#twitter"></use></svg
-                      ></a>
-                    </li>
-                    <li>
-                      <a href="" class="linkedin">
-                        <svg>
-                          <use
-                            xlink:href="/assets/images/icons.svg#linkedin"
-                          ></use></svg
-                      ></a>
-                    </li>
-                    <li>
-                      <a href="" class="whatsapp">
-                        <svg>
-                          <use
-                            xlink:href="/assets/images/icons.svg#whatsapp"
-                          ></use></svg
-                      ></a>
-                    </li>
-                  </ul>
-                </div> -->
-                    </div>
-                    <div class="single-page-content" data-aos="fade-up">
-                        <p>
-                            خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء
-                            وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة
-                            في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين
-                            وتنامت حين ٣٠, ونتج والحزب المذابح كل جوي. أسر كارثة المشتّتون
-                            بل, وبعض وبداية الصفحة غزو قد, أي بحث تعداد الجنوب.
-                        </p>
-
-                        <p>
-                            قصف المسرح واستمر الإتحاد في, ذات أسيا للغزو، الخطّة و, الآخر
-                            لألمانيا جهة بل. في سحقت هيروشيما البريطاني يتم, غريمه باحتلال
-                            الأيديولوجية، في فصل, دحر وقرى لهيمنة الإيطالية ٣٠. استبدال
-                            استسلام القاذفات عل مما. ببعض مئات وبلجيكا، قد أما, قِبل
-                            الدنمارك حتى كل, العمليات اليابانية انه أن.
-                        </p>
-                        <p>
-                            حتى هاربر موسكو ثم, وتقهقر المنتصرة حدة عل, التي فهرست واشتدّت
-                            أن أسر. كانت المتاخمة التغييرات أم وفي. ان وانتهاءً باستحداث
-                            قهر. ان ضمنها للأراضي الأوروبية ذات.
-                        </p>
-                        <p>
-                            حشد الثقيل المنتصر ثم, أسر قررت تم. وغير تصفح الحزب واستمر,
-                            مشروط الساحلية هذا ان. أما معركة لبلجيكا، من, الألوف الثقيلة
-                            الإنجليزية أسر ٣٠. ٣٠ دار أمام أحدث, أما بحشد الهادي الدولارات
-                            ما, هو الحزب الصفحة محاولات قبل. وبحلول الخنادق الأوروبية، ان
-                            غير, وليرتفع برلين، انه, انتباه الوزراء البولندي تم تلك.
-                        </p>
-                        <p>
-                            كما أن وقام وبدأت, لم أدوات للمجهود بلا. إذ لها الأول الستار,
-                            تحت وصغار مدينة عل. أي بحشد ليرتفع الساحلية أما, ليركز الهادي
-                            للأسطول ما هذا, أسابيع الروسية وتم عن. وفي مع شدّت فكان أدوات.
-                            سمّي تعداد ونستون هذا ما. به، بـ الخاصّة هيروشيما, وربع جندي
-                            الشهير الساحل.
-                        </p>
-                        <p>
-                            يكن لعدم الثانية عل, جديداً الخاطفة منشوريا بها تم, إذ جهة
-                            الأمم الجنوب. أي أما الحربية المعارك, قد وعلى الحربي، الأولية
-                            جعل. بحث إعادة قُدُماً ان, بحث أطراف استولت شموليةً ما. الغزو
-                            قبضتهم للسيطرة عدد أم. دون أي بالقصف العالم، للأسطول.
-                        </p>
-                        <p>
-                            مدن ثم للسيطرة سنغافورة, أفاق الاعتداء أخر ٣٠, لمّ أسيا غرّة،
-                            مع. هو ودول وجهان فقد, في الوراء وبالتحديد، غير. وألمّ وجهان
-                            به،, ان ربع حصدت وحزبه, أم جعل بشكل سابق الكونجرس. وضم يقوم
-                            الأولية شموليةً أن, أي ربع طرفاً الأرضية.
-                        </p>
-                        <p>
-                            ذلك بالفشل ونستون ابتدعها قد. لها قد مساعدة الحلفاء, واشتدّت
-                            الهزائم إلى كل. تم البلطيق الحيلولة دار, عن به، تُصب البرية
-                            والحلفاء. مشارف واشتدّت شبح كل, بتخصيص بل مما. الحرة بقيادة تم
-                            وصل.
-                        </p>
-                        <p>
-                            لغزو احتار كل أسر, بـ هُزم النمسا الخاسر بعد, من مسرح ألمانيا
-                            البشريةً فعل. والجنوب ارتكبها وبالتحديد، فعل. الا مع قِبل
-                            أمدها جديداً. بوابة الضغوط أن ولم. قد لمّ مكثّفة دنكيرك. جهة
-                            وبعض شعار ان.
-                        </p>
-                        <p>
-                            بحق نهاية تكاليف بريطانيا، ما, إلى أن النزاع الألماني. حرب
-                            غزوه أصقاع القوقازية تم, حتى كل ألماني بقيادة والكوري, بلا
-                            أجزاء مواقعها بل. عدد عقبت بالسيطرة عل. دول معقل لهذه أسابيع.
-                            أن وقد وباءت المجتمع, هجوم وبغطاء ذلك هو. تعديل فهرست.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="container pb-5 mb-5">
-    <div class="row justify-content-center">
-        <div class="col-md-12 col-lg-11 col-xl-10 col-xxl-9">
-            <div class="single-page-action" data-aos="fade-up">
-                <h2 data-aos="fade-up" data-aos-delay="300">
-                    يمكنك طلب الإستشارة بعدة خطوات بسيطة
-                </h2>
-                <div class="single-page-action-inner" data-aos="fade-up" data-aos-delay="600">
-                    <a data-bs-toggle="modal" data-bs-target="#SendEmail" class="btn btn-email" href="#">
-                        <svg>
-                            <use xlink:href="/assets/images/icons.svg#email"></use>
-                        </svg>
-                        <span>طلب الإستشارة </span>
-                    </a>
-                </div>
-            </div>
-        </div>
+<div class="payment-page">
+    <?= $invoice ?>
+    <ul class="details-list">
+        <li>
+            <strong>
+                الإسم
+            </strong>
+            <span>أمير العفش</span>
+        </li>
+        <li>
+            <strong>
+                الإسم
+            </strong>
+            <span>أمير العفش</span>
+        </li>
+        <li>
+            <strong>
+                الإسم
+            </strong>
+            <span>أمير العفش</span>
+        </li>
+        <li>
+            <strong>
+                الإسم
+            </strong>
+            <span>أمير العفش</span>
+        </li>
+        <li>
+            <strong>
+                الإسم
+            </strong>
+            <span>أمير العفش</span>
+        </li>
+        <li>
+            <strong>
+                الإسم
+            </strong>
+            <span>أمير العفش</span>
+        </li>
+    </ul>
+    <div class="btn-pay">
+        <a href="#" class="btn">الذهاب لصفحة الدفع</a>
     </div>
 </div>
 
