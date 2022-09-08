b6efccc2af12e70ecb2047dc5e08b92b457c6257
devameer <ameerafch@gmail.com>
2022-09-08 02:47:23 -0400

    add from host

--- invoice.php
+++ invoice.php
@@ -6,6 +6,9 @@ if (isset($_GET['invoice_id']) && !empty($_GET['invoice_id'])) {
         include('404.php');
         exit;
     }
+}else{
+    include('404.php');
+        exit;
 }
 
 include 'includes/header.php';
@@ -15,21 +18,33 @@ include 'includes/header.php';
 .navbar-offcanvas .logo img {
     filter: brightness(20.5);
 }
+footer {
+    display: none;
+}
+
+.nav-bar {
+    display: none;
+}
+
+.template-page-head {
+    min-height: 100vh;
+    display:flex;
+    align-items:center;
+}
 </style>
-<div class="template-page-head">
+<div class="template-page-head pb-0">
     <div class="container">
         <div class="row align-items-center" data-aos="fade-up" data-aos-delay="300">
             <div class="col-6">
                 <div class="text">
                     <h1>الدفع </h1>
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="/ ">الرئيسية</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">
-                                الدفع
-                            </li>
-                        </ol>
-                    </nav>
+                    <div class="price">
+                        <h3>السعر</h3>
+                        <span><?= $invoice['total_price'] ?> ريال</span>
+                    </div>
+                    <div class="btn-pay">
+                        <a href="<?= $invoice['next_url'] ?>" class="btn">الذهاب لصفحة الدفع</a>
+                    </div>
                 </div>
             </div>
             <div class="col-6">
@@ -40,50 +55,7 @@ include 'includes/header.php';
         </div>
     </div>
 </div>
-<div class="payment-page">
-    <?= $invoice ?>
-    <ul class="details-list">
-        <li>
-            <strong>
-                الإسم
-            </strong>
-            <span>أمير العفش</span>
-        </li>
-        <li>
-            <strong>
-                الإسم
-            </strong>
-            <span>أمير العفش</span>
-        </li>
-        <li>
-            <strong>
-                الإسم
-            </strong>
-            <span>أمير العفش</span>
-        </li>
-        <li>
-            <strong>
-                الإسم
-            </strong>
-            <span>أمير العفش</span>
-        </li>
-        <li>
-            <strong>
-                الإسم
-            </strong>
-            <span>أمير العفش</span>
-        </li>
-        <li>
-            <strong>
-                الإسم
-            </strong>
-            <span>أمير العفش</span>
-        </li>
-    </ul>
-    <div class="btn-pay">
-        <a href="#" class="btn">الذهاب لصفحة الدفع</a>
-    </div>
-</div>
+
 
 
 <?php include 'includes/footer.php' ?>
